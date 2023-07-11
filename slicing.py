3ad4eaa65053a284ff22a575e2a12942afccb744
Raviteja1229 <138431673+Raviteja1229@users.noreply.github.com>
2023-07-11 12:31:19 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,23 @@
+# slicing operation
+# syntax a[start index:end index:step value]
+
+a="ram is boy"
+print(a[0:6:1])
+
+a="ram is boy"
+print(a[0:8:2])
+
+a="ram is boy"
+print(a[:6:3])
+
+a="ram is boy"
+print(a[-1:-6:-1])
+
+a="ram is boy"
+print(a[-1:-6:-4])
+
+s="abcdefghijkl"
+print(s[8:-1:-2])
+
+s="abcdefghijkl"
+print(s[8:-1:2])
