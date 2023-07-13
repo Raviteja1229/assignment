562da20b028fc91bd5479447109507884e92ca7b
Raviteja <ravi.marolix2912@gmail.com>
2023-07-13 21:23:59 +0530

    initial commit

---
+++ break3.py
@@ -0,0 +1,5 @@
+numbers = [23,33,43,54,123,244,435,121,221]
+for num in numbers:
+    if num > 100:
+        print("the first number greater than 100 is :",num)
+        break
---
+++ break4.py
@@ -0,0 +1,7 @@
+for i in range(5):
+    for j in range(5):
+        if i ==2 and j == 2:
+            break
+        print(i,j)
+    if i == 3:
+        break
